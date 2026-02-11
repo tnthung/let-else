504f3d8288271a1baad95c7e4638d8e397e5ce3f
tnthung <hung11112002@gmail.com>
2026-02-11 09:22:49 +0800

    [Chg] enable the `else-match`

--- src/lib.rs
+++ src/lib.rs
@@ -16,63 +16,12 @@ use syn::parse::discouraged::Speculative;
 /// ```
 #[proc_macro]
 pub fn let_else(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-  enum RestPat {
-    Pattern(syn::Pat),
-    Ident(syn::Ident),
-  }
+  let Input { pattern, expr, rest, .. } = syn::parse_macro_input!(input as Input);
 
-  impl syn::parse::Parse for RestPat {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-      if let Err(_) = input.parse::<syn::Token![as]>() {
-        return Ok(Self::Pattern(syn::parse_quote! { _ }));
-      }
-
-      let fork = input.fork();
-      if let Ok(pattern) = fork.call(syn::Pat::parse_multi_with_leading_vert) {
-        input.advance_to(&fork);
-        return Ok(Self::Pattern(pattern));
-      }
-
-      if let Ok(ident) = input.parse() {
-        return Ok(Self::Ident(ident));
-      }
-
-      Err(input.error("Expected a pattern or an identifier after `as`"))
-    }
-  }
-
-  impl quote::ToTokens for RestPat {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-      match self {
-        RestPat::Pattern(pat) => pat.to_tokens(tokens),
-        RestPat::Ident(ident) => ident.to_tokens(tokens),
-      }
-    }
-  }
-
-  struct Input {
-    pattern:  syn::Pat,
-    _sym_eq:  syn::Token![=],
-    expr:     syn::Expr,
-    _kw_else: syn::Token![else],
-    rest:     RestPat,
-    block:    syn::Block,
-  }
-
-  impl syn::parse::Parse for Input {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-      Ok(Self {
-        pattern:  input.call(syn::Pat::parse_single)?,
-        _sym_eq:  input.parse()?,
-        expr:     input.parse()?,
-        _kw_else: input.parse()?,
-        rest:     input.parse()?,
-        block:    input.parse()?,
-      })
-    }
-  }
-
-  let Input { pattern, expr, rest, block, .. } = syn::parse_macro_input!(input as Input);
+  let match_inner = match rest {
+    Rest::Simple(SimpleRest { bound, block }) => quote! { #bound => #block, },
+    Rest::Complete(CompleteRest { arms, .. }) => quote! { #(#arms)* },
+  };
 
   quote! {
     #[doc(hidden)]
@@ -81,8 +30,128 @@ pub fn let_else(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
       match __let_else_expr__ {
         #[allow(unused, dead_code)]
         #pattern => unsafe { std::hint::unreachable_unchecked() },
-        #rest => #block,
+        #match_inner
       }
     };
   }.into()
+}
+
+
+struct Input {
+  pattern:  syn::Pat,
+  _sym_eq:  syn::Token![=],
+  expr:     syn::Expr,
+  _kw_else: syn::Token![else],
+  rest:     Rest,
+}
+
+impl syn::parse::Parse for Input {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    Ok(Self {
+      pattern:  input.call(syn::Pat::parse_single)?,
+      _sym_eq:  input.parse()?,
+      expr:     input.parse()?,
+      _kw_else: input.parse()?,
+      rest:     input.parse()?,
+    })
+  }
+}
+
+
+enum Rest {
+  Simple(SimpleRest),
+  Complete(CompleteRest),
+}
+
+impl syn::parse::Parse for Rest {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let fork = input.fork();
+    if let Ok(simple) = fork.parse() {
+      input.advance_to(&fork);
+      return Ok(Self::Simple(simple));
+    }
+
+    let fork = input.fork();
+    if let Ok(complete) = fork.parse() {
+      input.advance_to(&fork);
+      return Ok(Self::Complete(complete));
+    }
+
+    Err(input.error("Expected a simple or complete rest block"))
+  }
+}
+
+
+struct SimpleRest {
+  bound: RestBound,
+  block: syn::Block,
+}
+
+impl syn::parse::Parse for SimpleRest {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    Ok(Self {
+      bound: input.parse()?,
+      block: input.parse()?,
+    })
+  }
+}
+
+
+enum RestBound {
+  Pattern(syn::Pat),
+  Ident(syn::Ident),
+}
+
+impl syn::parse::Parse for RestBound {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    if let Err(_) = input.parse::<syn::Token![as]>() {
+      return Ok(Self::Pattern(syn::parse_quote! { _ }));
+    }
+
+    let fork = input.fork();
+    if let Ok(pattern) = fork.call(syn::Pat::parse_multi_with_leading_vert) {
+      input.advance_to(&fork);
+      return Ok(Self::Pattern(pattern));
+    }
+
+    if let Ok(ident) = input.parse() {
+      return Ok(Self::Ident(ident));
+    }
+
+    Err(input.error("Expected a pattern or an identifier after `as`"))
+  }
+}
+
+impl quote::ToTokens for RestBound {
+  fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+    match self {
+      RestBound::Pattern(pat) => pat.to_tokens(tokens),
+      RestBound::Ident(ident) => ident.to_tokens(tokens),
+    }
+  }
+}
+
+
+struct CompleteRest {
+  kw_match: syn::Token![match],
+  arms:     Vec<syn::Arm>,
+}
+
+impl syn::parse::Parse for CompleteRest {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    Ok(Self {
+      kw_match: input.parse()?,
+      arms: {
+        let content;
+        syn::braced!(content in input);
+
+        let mut arms = Vec::new();
+        while !content.is_empty() {
+          arms.push(content.parse()?);
+        }
+
+        arms
+      },
+    })
+  }
 }
