6fe1ef21d04f88979ac343ad7b42293b36442b34
tnthung <hung11112002@gmail.com>
2026-02-11 09:22:59 +0800

    [Add] more tests

--- tests/main.rs
+++ tests/main.rs
@@ -44,3 +44,73 @@ fn test3() {
   assert_eq!(foo(Ok(42)), "Value: 42");
   assert_eq!(foo(Err("Something went wrong".to_string())), "Error: Err(\"Something went wrong\")");
 }
+
+
+#[test]
+fn test4() {
+  #[allow(dead_code)]
+  enum Triple {
+    A(i32),
+    B(String),
+    C(f64),
+  }
+
+  fn foo(value: Triple) -> String {
+    let_else!(Triple::A(value) = value else as Triple::B(_) | Triple::C(_) {
+      return format!("Error: B or C");
+    });
+
+    return format!("Value: {}", value);
+  }
+
+  assert_eq!(foo(Triple::A(42)), "Value: 42");
+  assert_eq!(foo(Triple::B("Hello".to_string())), "Error: B or C");
+  assert_eq!(foo(Triple::C(3.14)), "Error: B or C");
+}
+
+
+#[test]
+fn test5() {
+  #[allow(dead_code)]
+  enum Triple {
+    A(i32),
+    B(String),
+    C(f64),
+  }
+
+  fn foo(value: Triple) -> String {
+    let_else!(Triple::A(value) = value else match {
+      Triple::B(b) => { return format!("Error: B with value {}", b); },
+      Triple::C(c) => { return format!("Error: C with value {}", c); },
+    });
+
+    return format!("Value: {}", value);
+  }
+
+  assert_eq!(foo(Triple::A(42)), "Value: 42");
+  assert_eq!(foo(Triple::B("Hello".to_string())), "Error: B with value Hello");
+  assert_eq!(foo(Triple::C(3.14)), "Error: C with value 3.14");
+}
+
+
+#[test]
+fn test6() {
+  #[allow(dead_code)]
+  enum Triple {
+    A(i32),
+    B(u32),
+    C(u32),
+  }
+
+  fn foo(value: Triple) -> String {
+    let_else!(Triple::A(value) = value else as Triple::B(y) | Triple::C(y) {
+      return format!("Error: B or C with value {}", y);
+    });
+
+    return format!("Value: {}", value);
+  }
+
+  assert_eq!(foo(Triple::A(42)), "Value: 42");
+  assert_eq!(foo(Triple::B(100)), "Error: B or C with value 100");
+  assert_eq!(foo(Triple::C(200)), "Error: B or C with value 200");
+}
